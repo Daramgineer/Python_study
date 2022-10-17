944afc3f7ef1bdfb06071a1df67d14623e6fb9e5
JW Cha <cjw950321@gmail.com>
2022-10-17 23:33:43 +0900

    tomorrow subject

---
+++ Python_study/GUI_tkinter/gui_basic/15_quize.py
@@ -0,0 +1,13 @@
+#1. 메뉴:파일, 편집, 서식, 보기, 도움말
+#2. 실제 메뉴 구현: 파일메뉴에서 열기, 저장, 끝내기
+#3. 열기:mynote.txt파일 내용 열어서 보여주기
+#4. 저장:mynote.txt파일에 현재 내용 저장하기
+#5. 끝내기:종료
+
+from tkinter import*
+
+root = Tk()
+root.title('JW GUI')
+root.geometry('640x480')
+
+root.mainloop()
