a20ba943969a4424517efd245b18d3d80a5b91f8
Daramgieer <prince2667@naver.com>
2022-11-28 19:22:48 +0900

    add

---
+++ Python_study/4th.Code-Test/3.Iteration/5.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+i = 0
+j = 0
+x = [0 for i in range(T)]
+while i < T:
+    a, b = map(int, input().split())
+    x[i] = a+b
+    i += 1
+while j < T:
+    print("{0}".format(x[j]))
+    j += 1
