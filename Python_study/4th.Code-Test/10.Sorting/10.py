6f973d5e55ffa348a9881d87c47f782bd327d9eb
Daramgieer <cjw950321@gmail.com>
2022-12-07 20:42:34 +0900

    add recursion test

--- Python_study/4th.Code-Test/10.Sorting/10.py
+++ Python_study/4th.Code-Test/10.Sorting/10.py
@@ -4,6 +4,7 @@ l = []
 
 for _ in range(n):
     l.append(sys.stdin.readline().strip())
+    
 set_list = set(l)	## set으로 변환해서 중복값을 제거
 l = list(set_list)	## 다시 list로 변환
 l.sort()	#괄호 안에 아무 값도 넣지 않으면 알파벳 순서대로 정렬.
---
+++ Python_study/4th.Code-Test/11.Recursion/1.py
@@ -0,0 +1,6 @@
+a = int(input())
+n = 1
+while a > 0:
+    n *= a
+    a -=1
+print(n)
